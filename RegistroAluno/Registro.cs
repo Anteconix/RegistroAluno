c4f14ac8d0e473ee61ae14618105fbfb638a3c48
Anteconix <magnofilho2323@gmail.com>
2024-04-09 22:05:41 -0300

    Erros

--- RegistroAluno/Registro.cs
+++ RegistroAluno/Registro.cs
@@ -1,17 +1,18 @@
-﻿//O cadastro de um novo aluno
-//deve ser realizado com seu nome, número de matrícula, e-mail e
-//telefone de contato.
-namespace Dominio
+﻿namespace Dominio
 {
     public class Registro
     {
         private string nome;
         private string numMatricula;
         private string email;
-        private int telefone;
+        private string telefone;
 
-        public Registro(string nome, string numMatricula, string email, int telefone)
+        public Registro(string nome, string numMatricula, string email, string telefone)
         {
+            if (string.IsNullOrEmpty(numMatricula)) throw new ArgumentException();
+            if (string.IsNullOrEmpty(nome)) throw new ArgumentException();
+            if (string.IsNullOrEmpty(email)) throw new ArgumentException();
+
             this.Nome = nome;
             this.NumMatricula = numMatricula;
             this.Email = email;
@@ -21,6 +22,6 @@ namespace Dominio
         public string Nome { get => nome; set => nome = value; }
         public string NumMatricula { get => numMatricula; set => numMatricula = value; }
         public string Email { get => email; set => email = value; }
-        public int Telefone { get => telefone; set => telefone = value; }
+        public string Telefone { get => telefone; set => telefone = value; }
     }
 }
--- Teste/RegistroTeste.cs
+++ Teste/RegistroTeste.cs
@@ -6,39 +6,88 @@ namespace Teste
 { 
     public class RegistroTeste
     {
-        private string    _nome;
-        private string    _numMatricula;
-        private string    _email;
-        private int       _telefone;
+        private string       _nome;
+        private string       _numMatricula;
+        private string       _email;
+        private string       _telefone;
 
-        Faker faker = new Faker();
         public RegistroTeste()
         {
-            this._nome = "Bryann";
-            this._numMatricula = faker.Random.Int(1000, 9999).ToString() + "-" +
-                                 faker.Random.Int(2010, 2024).ToString() + "-" +
-                                 faker.Random.Int(1, 2).ToString();
-            this._email = "branncena@penis.com";
-            this._telefone = 69696669;
+            Faker faker = new Faker();
+
+            _nome = faker.Person.FullName;
+            _numMatricula = faker.Random.Int(1000, 9999).ToString() + "-" +
+                            faker.Random.Int(2010, 2024).ToString() + "-" +
+                            faker.Random.Int(1, 2).ToString();
+            _email = faker.Person.Email;
+            _telefone = faker.Person.Phone;
         }
 
         [Fact]
 
         public void CriarObjetoRegistro()
         {
-            var RegistroEsperado = new
+            var registroEsperado = new
             {
                 Nome = _nome,
                 NumMatricula = _numMatricula,
                 Email = _email,
                 Telefone = _telefone
             };
-            Registro registro = new Registro(
-                _nome, _numMatricula,
-                _email, _telefone
-            );  
+
+            Registro registro = new Registro( _nome, _numMatricula, _email, _telefone);  
             
-            RegistroEsperado.ToExpectedObject().ShouldMatch( registro );
+            registroEsperado.ToExpectedObject().ShouldMatch( registro );
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+
+        public void MatriculaObrigatoria(string numMatriculaErrada)
+        {
+            Assert.Throws<ArgumentException>(
+                () =>
+                new Registro(numMatriculaErrada, _nome, _email, _telefone)
+                );
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+
+        public void NomeObrigatorio(string nomeErrado)
+        {
+            Assert.Throws<ArgumentException>(
+                () =>
+                new Registro(_numMatricula, nomeErrado, _email, _telefone)
+                );
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+
+        public void EmailObrigatorio(string emailErrado)
+        {
+            Assert.Throws<ArgumentException>(
+                () =>
+                new Registro( _numMatricula, emailErrado, _nome, _telefone)
+                );
+        }
+
+        [Theory]
+        [InlineData("teste.com")]
+        [InlineData("xxx.com.br")]
+        [InlineData("teste8")]
+        [InlineData("xunda@.br")]
+
+        public void EmailInvalido(string emailErrado)
+        {
+            Assert.Throws<ArgumentException>(
+                () =>
+                new Registro(_numMatricula, emailErrado, _nome, _telefone)
+                );
         }
     }
 }
